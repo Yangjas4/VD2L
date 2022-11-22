e433c5d96f93f287e33d9b0d0516490eb544d3db
Jason Yang <Jasonyang602@gmail.com>
2022-11-21 22:52:31 -0500

    styling mobile landing page

--- vd2l/src/Components/Navbar.jsx
+++ vd2l/src/Components/Navbar.jsx
@@ -1,16 +1,33 @@
 import Menu from "../assets/HamburgerMenu.svg"
+import { useState } from "react";
+
 
 export default function Navbar() {
+
+    
 	return (
 		<div className="navbar">
             <div className="top-blue-header"></div>
 			<div className="nav-items">
 				<h3 className="vd2l-text-logo">VD2L</h3>
-				<nav>
-                    
+				<nav className="desktop-nav-bar">
+                    <p className="nav-item">Home</p>
+                    <p className="nav-item">Rules</p>
+                    <p className="nav-item">Season 0</p>
+                    <p className="nav-item">Inhouse</p>
                 </nav>
                 <img src={Menu} alt="Hamburger Menu Icon" className="hamburger-menu-icon" />
 			</div>
+            <div className="hamburger-menu-background"></div>
+            <div className="hamburger-menu-sidebar">
+                <p className="x-button">x</p>
+                <nav>
+                    <p className="nav-item">Home</p>
+                    <p className="nav-item">Rules</p>
+                    <p className="nav-item">Season 0</p>
+                    <p className="nav-item">Inhouse</p>
+                </nav>
+            </div>
 		</div>
 	);
 }
--- vd2l/src/Pages/Landing.jsx
+++ vd2l/src/Pages/Landing.jsx
@@ -1,52 +1,74 @@
-import Navbar from "../Components/Navbar"
-import Footer from "../Components/Footer"
-import Valorant from "../assets/valorant.svg"
-import Signup from "../assets/Signup.svg"
-import DottoreDesktop from "../assets/DottoreDesktop.svg"
-import DottoreMobile from "../assets/DottoreMobile.svg"
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Valorant from "../assets/valorant.svg";
+import Signup from "../assets/Signup.svg";
+import DottoreDesktop from "../assets/DottoreDesktop.svg";
+import DottoreMobile from "../assets/DottoreMobile.svg";
 
 export default function Landing() {
+	return (
+		<div className="landing-page">
+			<Navbar />
+			<div className="content-container">
+				<div className="card">
+					<div className="card-container">
+						<div className="card-header">
+							<div className="card-header-text">
+								<p className="current-season">
+									Current Season:
+								</p>
+								<h1 className="season-no">Season 0</h1>
+							</div>
+							<img
+								src={Valorant}
+								alt="Valorant Logo"
+								className="valorant-logo"
+							/>
+						</div>
 
-    return (
-        <div className="landing-page">
-            <Navbar />
-            <div className="content-container">
-                <div className="card">
+						<div className="card-body">
+							<h1 className="signup-status">
+								Signups are currently{" "}
+								<span className="red">OPEN</span>
+							</h1>
+							<div className="signup-button">
+								<div className="signup-button-content">
+									<p className="signup-button-text">
+										Sign Up
+									</p>
+									<img
+										src={Signup}
+										alt="Signup Logo"
+										className="signup-logo"
+									/>
+								</div>
+							</div>
+							<div className="signup-dates">
+								<div className="start-date">
+									<div className="date-title">Start Date</div>
+									<div className="date">TBD</div>
+								</div>
+								<div className="end-date">
+									<div className="date-title">End Date</div>
+									<div className="date">TBD</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-                    <div className="card-header">
-                        <div className="card-header-text">
-                            <p className="current-season">Current Season:</p>
-                            <h1 className="season-no">Season 0</h1>
-                        </div>
-                        <img src={Valorant} alt="Valorant Logo" className="valorant-logo" />
-                    </div>
-
-                    <div className="card-body">
-                        <h1 className="signup-status">Signups are currently <span className="red">OPEN</span></h1>
-                        <div className="signup-button">
-                            <div className="signup-button-content">
-                                <h4 className="signup-button-text">Sign Up</h4>
-                                <img src={Signup} alt="Signup Logo" className="signup-logo" />
-                            </div>
-                        </div>
-                        <div className="signup-dates">
-                            <div className="start-date">
-                                <div className="date-title">Start Date</div>
-                                <div className="date">TBD</div>
-                            </div>
-                            <div className="end-date">
-                                <div className="date-title">End Date</div>
-                                <div className="date">TBD</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <img src={DottoreMobile} alt="Hero Picture" className="dottore-mobile" />
-                <img src={DottoreDesktop} alt="Hero Picture" className="dottore-desktop" />
-            </div>
-            <Footer />
-        </div>
-    )
-
+				<img
+					src={DottoreMobile}
+					alt="Hero Picture"
+					className="dottore-mobile"
+				/>
+				<img
+					src={DottoreDesktop}
+					alt="Hero Picture"
+					className="dottore-desktop"
+				/>
+			</div>
+			<Footer />
+		</div>
+	);
 }
